c938f4315d19f0a2a9e1578e6332acf34963dbb0
Aarni Koskela <akx@iki.fi>
2022-03-21 16:56:15 +0200

    Obliterate type comment

--- laituri/docker/credential_manager/credential_managers.py
+++ laituri/docker/credential_manager/credential_managers.py
@@ -5,7 +5,9 @@ from laituri.docker.credential_manager.registry_credentials_callback_v1 import (
     registry_credentials_callback_v1_credential_manager,
 )
 
-credential_managers = {
+NameAndVersion = Tuple[str, str]
+
+credential_managers: Dict[NameAndVersion, Any] = {
     ('docker', '1'): docker_v1_credential_manager,
     ('registry-credentials-callback', '1'): registry_credentials_callback_v1_credential_manager,
-}  # type: Dict[Tuple[str, str], Any]
+}
