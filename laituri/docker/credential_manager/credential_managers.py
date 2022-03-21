004c5f2d555da7564d682361013dd12f5505872b
Aarni Koskela <akx@iki.fi>
2022-03-21 16:54:37 +0200

    Normalize relative imports to absolute imports

--- laituri/docker/credential_manager/credential_managers.py
+++ laituri/docker/credential_manager/credential_managers.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict, Tuple
 
-from .docker_v1 import docker_v1_credential_manager
-from .registry_credentials_callback_v1 import registry_credentials_callback_v1_credential_manager
+from laituri.docker.credential_manager.docker_v1 import docker_v1_credential_manager
+from laituri.docker.credential_manager.registry_credentials_callback_v1 import (
+    registry_credentials_callback_v1_credential_manager,
+)
 
 credential_managers = {
     ('docker', '1'): docker_v1_credential_manager,
