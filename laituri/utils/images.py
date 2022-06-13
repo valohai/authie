4682324038e5410dd5044ab477f186fe738b6186
magdapoppins <stenius.mimmy@gmail.com>
2022-06-13 09:37:42 +0300

    Extend hostname resolution to correctly resolve localhost and images with no owner name and custom registries

--- laituri/utils/images.py
+++ laituri/utils/images.py
@@ -1,2 +1,6 @@
 def get_image_domain(image: str) -> str:
-    return image.split('/')[0] if image.count('/') > 1 else 'docker.io'
+    if image.count('/'):
+        first = image.split('/')[0]
+        if '.' in first or ':' in first or first == 'localhost':
+            return first
+    return 'docker.io'
--- laituri_tests/test_utils.py
+++ laituri_tests/test_utils.py
@@ -3,4 +3,8 @@ from laituri.utils.images import get_image_domain
 
 def test_get_image_domain():
     assert get_image_domain("python:3.6") == "docker.io"
+    assert get_image_domain("tensorflow/tensorflow:latest") == "docker.io"
+    assert get_image_domain("docker.io/tensorflow/tensorflow:latest") == "docker.io"
+    assert get_image_domain("my.io/tensorflow:latest") == "my.io"
+    assert get_image_domain("localhost:500/tensorflow:latest") == "localhost:500"
     assert get_image_domain("gcr.io/google-containers/busybox:latest") == "gcr.io"
