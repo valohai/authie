52e6bcdc31ff44d4ab9471a66ae3197a4ffbbd20
magdapoppins <stenius.mimmy@gmail.com>
2022-09-14 11:26:10 +0300

    Allow using AWS instance roles for docker login

--- laituri/docker/credential_manager/credential_managers.py
+++ laituri/docker/credential_manager/credential_managers.py
@@ -1,6 +1,7 @@
 from typing import Dict, Tuple
 
 from laituri.docker.credential_manager.docker_v1 import docker_v1_credential_manager
+from laituri.docker.credential_manager.ecr_with_role_v1 import ecr_with_role_v1_credential_manager
 from laituri.docker.credential_manager.registry_credentials_callback_v1 import (
     registry_credentials_callback_v1_credential_manager,
 )
@@ -11,4 +12,5 @@ NameAndVersion = Tuple[str, str]
 credential_managers: Dict[NameAndVersion, CredentialManagerFactory] = {
     ('docker', '1'): docker_v1_credential_manager,
     ('registry-credentials-callback', '1'): registry_credentials_callback_v1_credential_manager,
+    ('ecr-with-role', '1'): ecr_with_role_v1_credential_manager,
 }
